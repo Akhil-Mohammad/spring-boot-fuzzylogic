6ae3d2ecd2151904648fbd4804e8494cb0738e0e
AkhilMohammad <aakhilmohammad@gmail.com>
2024-09-07 19:27:40 +0530

    started working on fuzzylogic in java

---
+++ src/main/java/com/springboot/fuzzylogic/FuzzylogicApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fuzzylogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzylogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzylogicApplication.class, args);
+	}
+
+}
